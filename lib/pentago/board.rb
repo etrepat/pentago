017be3557294a51fed373b134cca2e84fc089bf9
Estanislau Trepat <estanis@etrepat.com>
2011-01-19 16:19:57 +0100

    added preliminar Board implementation + some tests

--- lib/pentago/board.rb
+++ lib/pentago/board.rb
@@ -1,6 +1,75 @@
 module Pentago
   class Board
-    # add code here...
+    class BoardError < StandardError; end
+    class IllegalPositionError < BoardError; end
+
+    ROTATION_DIRECTIONS = [:clockwise, :counter_clockwise]
+
+    ROTATION_MATRICES = {
+      :clockwise => [2,7,12,-5,0,5,-12,-7,-2],
+      :counter_clockwise => [12,5,-2,7,0,-7,2,-5,-12]
+    }
+
+    SQUARES = [
+      [ 0, 1, 2, 6, 7, 8,12,13,14],
+	    [ 3, 4, 5, 9,10,11,15,16,17],
+	    [18,19,20,24,25,26,30,31,32],
+	    [21,22,23,27,28,29,33,34,35]
+    ]
+
+    CELLS = 36
+
+    def initialize(options={})
+      @squares = Array.new(CELLS, 0)
+      initialize_from_previous_state(options[:board]) if options[:board]
+    end
+
+    attr_reader :squares
+
+    def place_marker(x, y, marker)
+      pos = x + 6*y
+      raise IllegalPositionError, "Illegal position [#{x}, #{y}]" unless squares[pos]
+      raise IllegalPositionError, "Position already occupied" if squares[pos] != 0
+
+      @squares[pos] = marker
+    end
+
+    def to_s
+      output = "\n"
+      squares.each_with_index do |value, index|
+        if index != 0
+          if index % 6 == 0
+            output << "\n"
+          elsif index % 3 == 0
+            output << "|"
+          end
+
+				  output << "---------+---------\n" if index % 18 == 0
+        end
+
+        output << (value != 0 ? " #{value} " : ' . ')
+      end
+
+      output
+    end
+
+    def to_a
+      squares
+    end
+
+    protected
+
+    def position(x, y)
+      x + y*6
+    end
+
+    def initialize_from_previous_state(board)
+      if board.is_a?(Array) || board.is_a?(Pentago::Board)
+        @squares = board.dup.to_a
+      else
+        raise TypeError, 'Incompatible types'
+      end
+    end
   end
 end
 
--- spec/pentago/board_spec.rb
+++ spec/pentago/board_spec.rb
@@ -2,9 +2,57 @@ require 'spec_helper'
 
 module Pentago
   describe Board do
-    it 'should consist of 6x6 locations'
-    it 'should allow placing a marker'
-    # add specs here...
+    describe '#initialize' do
+      it 'should create an empty board' do
+        board = Board.new
+        board.squares.should == Array.new(Board::CELLS, 0)
+      end
+
+      it 'should create a board based on a previous state' do
+        # from array
+        previous = Array.new(Board::CELLS, 0)
+        6.times do |n|
+          pos = rand(Board::CELLS+1)
+          pos = rand(Board::CELLS+1) while previous[pos] != 0
+          previous[pos] = 1 # at this point we don't mind about players
+        end
+
+        board = Board.new(:board => previous)
+        board.squares.should == previous
+
+        # from another board
+        board2 = Board.new(:board => board)
+        board2.squares.should == board.squares
+      end
+    end
+
+    describe '#place_marker' do
+      it 'should let us place a marker' do
+        board = Board.new
+        board.place_marker(1, 2, 1)
+        board.squares[13].should == 1
+      end
+
+      it 'should raise IllegalPositionError when out of bounds' do
+        board = Board.new
+        expect {
+          board.place_marker(6, 8, 1)
+        }.to raise_error(Pentago::Board::IllegalPositionError)
+      end
+
+      it 'should raise IllegalPositionError if previously occupied' do
+        board = Board.new
+        expect do
+          board.place_marker(3, 2, 1)
+          board.place_marker(3, 2, 1)
+        end.to raise_error(Pentago::Board::IllegalPositionError)
+      end
+    end
+
+    describe '#rotate' do
+      it 'should allow us to rotate a square CW'
+      it 'should allow us to rotate a square CCW'
+    end
   end
 end
 
