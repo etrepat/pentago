8ed002b2d7e2433353743b1df21d3b32ebe4be45
Estanislau Trepat <estanis@etrepat.com>
2011-01-25 13:16:36 +0100

    implemented ask_for_move callback + other logic to ask for moves via de console. preliminar version of a human player working.

--- lib/pentago/ui/console.rb
+++ lib/pentago/ui/console.rb
@@ -7,22 +7,25 @@ module Pentago
         @input      = stdin
         @output     = stdout
       end
-      
+
       def run
         if parsed_options? && options_valid?
           @board  = Pentago::Board.new
           @game   = Pentago::Game.new(@options[:player1], @options[:player2], @board)
           @game.add_observer(self)
-          
+
           @game.play
         else
           output_usage
         end
       end
-      
+
       def update(player_at_turn, board)
-        marble  = player_at_turn.marble
-        x, y, s, d = player_at_turn.last_move
+        # get info
+        marble      = player_at_turn.marble
+        x, y, s, d  = player_at_turn.last_move
+
+        # output turn info & board
         say "\nTurn #{board.moves}: Player #{marble} played: [#{x}, #{y}], rotated square: #{s} #{d}"
         if board.game_over?
           if board.tie?
@@ -31,11 +34,28 @@ module Pentago
             say "Game ends w/state: --- PLAYER #{board.find_winner} WINS! ---"
           end
         end
-        say "-------------------"
+
         say board
-        say "-------------------"
       end
-      
+
+      def ask_for_move(player, board)
+        say board if board.moves == 0
+        say "Player #{player.marble}. It's your Turn."
+
+        x, y = nil, nil
+        while x.nil? && y.nil?
+          x, y = ask_coords("Enter marble position")
+        end
+
+        s, d = nil, nil
+        direction_mapping = { 'cw' => :clockwise, 'ccw' => :counter_clockwise }
+        while s.nil? && d.nil?
+          s, d = ask_rotation("Enter square and direction to rotate")
+        end
+
+        [x, y, s, direction_mapping[d]]
+      end
+
       def parsed_options?
         @options_parser ||= OptionParser.new do |opts|
           opts.banner = <<BANNER
@@ -43,25 +63,25 @@ Pentago Bot/Game AI Console Lab v#{Pentago.version}
 
 Usage:
   pentago --player1=[Engine] --player2=[Engine] ([options])
-  
+
 Example:
   pentago --player1=Human --player2=Minimax
-  
+
 Options are:
 BANNER
           opts.on('--player1=PLAYER', String,
             'Player 1 Engine (required)') do |engine|
               @options[:player1] = load_player(engine, 1)
           end
-          
+
           opts.on('--player2=PLAYER', String,
             'Player 2 Engine (required)') do |engine|
               @options[:player2] = load_player(engine, 2)
           end
-          
+
           # TODO: option for reading a board from a text file
           # TODO: option to set level of dificulty (of course, AI should be done first)
-          
+
           opts.separator ''
 
           opts.on('-h', '--help', 'Show this message') do
@@ -71,7 +91,7 @@ BANNER
 
           opts.separator ''
         end
-        
+
         begin
           @options_parser.parse!(@arguments)
         rescue TypeError, OptionParser::ParseError => e
@@ -79,46 +99,58 @@ BANNER
           nil
         end
       end
-      
+
       protected
-      
+
       def options_valid?
         @options.keys.include?(:player1) && @options.keys.include?(:player2)
       end
-      
+
       def output_usage
         puts @options_parser
       end
-      
+
       def load_player(engine_name, player_marble)
-        player = player_to_constant(engine_name).new(player_marble) unless player.kind_of?(Pentago::Players::Base)
+        player = player_to_constant(engine_name).new(player_marble)
+        raise TypeError if player.instance_of?(Pentago::Players::Base)
+
+        if player.kind_of?(Pentago::Players::Human)
+          player.ask_for_move_callback = lambda { |player, board| ask_for_move(player, board) }
+        end
+
+        player
       rescue Exception => e
-        puts e.message
-        raise TypeError, "invalid player engine (#{engine_name})"       
+        raise TypeError, "#{e.message}\n#{e.backtrace}\n!! invalid player engine (#{engine_name})"
       end
-      
+
       def player_to_constant(name)
         camel = name.to_s.split('_').map { |s| s.capitalize }.join
         eval("Pentago::Players::#{camel}")
-      end  
-      
+      end
+
       def gets
         @input ? @input.gets : ''
       end
-      
+
       def say(message)
         @output.puts(message) if @output
       end
-      
+
       def ask(question)
         say("#{question} [yn]: ")
         gets.chomp.downcase == 'y'
       end
-      
+
       def ask_coords(question)
-        say("#{question} [x,y] (ex: 3,4 - comma sep.): ")
+        say("#{question} [ex: 3,4]: ")
         gets.chomp.split(',').map { |n| n.strip.to_i }
+      end
+
+      def ask_rotation(question)
+        say("#{question} [ex: 1,cw || 0,ccw]: ")
+        gets.chomp.split(',').map { |n| n.strip }.map{ |n| n.start_with?('c') ? n : n.to_i }
       end
     end
   end
 end
+
