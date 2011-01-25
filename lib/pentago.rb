f8d34f7c7eac8af2486f4e20d2b937f4cd346e09
Estanislau Trepat <estanis@etrepat.com>
2011-01-25 02:41:34 +0100

    modified to require new dependencies

--- lib/pentago.rb
+++ lib/pentago.rb
@@ -1,3 +1,5 @@
+require 'observer'
+
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require
@@ -5,5 +7,10 @@ Bundler.require
 module Pentago; end
 
 require_relative 'pentago/board'
+require_relative 'pentago/game'
 require_relative 'pentago/version'
 
+require_relative 'pentago/players/base'
+require_relative 'pentago/players/dummy'
+
+require_relative 'pentago/ui/console'
