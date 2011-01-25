bb7eabadfe20deec3d2fb4b33545d8ecec93985e
Estanislau Trepat <estanis@etrepat.com>
2011-01-25 02:40:41 +0100

    added game class

---
+++ spec/pentago/game_spec.rb
@@ -0,0 +1,28 @@
+module Pentago
+  describe Game do
+    before(:each) do
+      @player1  = Pentago::Players::Dummy.new(1)
+      @player2  = Pentago::Players::Dummy.new(2)
+      @board    = Pentago::Board.new
+      
+      @game     = Pentago::Game.new(@player1, @player2, @board)
+    end
+    
+    describe '#initialize' do
+      it 'should hold player representations' do
+        @game.player1.should == @player1
+        @game.player2.should == @player2
+      end
+      
+      it 'should hold a board object' do
+        @game.board.should == @board
+      end
+    end
+    
+    describe '#play' do
+      it 'should alternatively make players play their turn' do
+        
+      end
+    end
+  end
+end
