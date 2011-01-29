1b89f9c294e3e4865a68845013b10d6038e7b072
Estanislau Trepat <estanis@etrepat.com>
2011-01-30 00:20:23 +0100

    modified console code to use highline to ask for human player moves

--- lib/pentago/ui/console.rb
+++ lib/pentago/ui/console.rb
@@ -6,55 +6,22 @@ module Pentago
         @options    = {}
         @input      = stdin
         @output     = stdout
+        @terminal   = HighLine.new(@input, @output)
       end
 
       def run
         if parsed_options? && options_valid?
-          @board  = Pentago::Board.new
-          @game = Pentago::Game.new(:player1 => @options[:player1], 
-            :player2 => @options[:player2], :board => @board)
+          @game = Pentago::Game.new(
+            :player1  => @options[:player1], 
+            :player2  => @options[:player2], 
+            :board    => @options[:board] || Pentago::Board.new
+          )
           @game.add_observer(self)
-
+          
           @game.play
         else
           output_usage
         end
-      end
-
-      def update(game)
-        # get info
-        marble      = game.current_player.marble
-        x, y, s, d  = game.current_player.last_move
-
-        # output turn info & board
-        say "\nTurn #{game.turns_played}: Player #{marble} played: [#{x}, #{y}], rotated square: #{s} #{d}"
-        if game.game_over?
-          if game.tie_game?
-            say "Game ends w/state: --- TIE GAME ---"
-          else
-            say "Game ends w/state: --- PLAYER #{game.find_winner} WINS! ---"
-          end
-        end
-
-        say game.board
-      end
-
-      def ask_for_move(player, board)
-        say board if board.moves == 0
-        say "Player #{player.marble}. It's your Turn."
-
-        x, y = nil, nil
-        while x.nil? && y.nil?
-          x, y = ask_coords("Enter marble position")
-        end
-
-        s, d = nil, nil
-        direction_mapping = { 'cw' => :clockwise, 'ccw' => :counter_clockwise }
-        while s.nil? && d.nil?
-          s, d = ask_rotation("Enter square and direction to rotate")
-        end
-
-        [x, y, s, direction_mapping[d]]
       end
 
       def parsed_options?
@@ -81,12 +48,12 @@ BANNER
           end
 
           # TODO: option for reading a board from a text file
-          # TODO: option to set level of dificulty (of course, AI should be done first)
+          # TODO: option to set level of dificulty
 
           opts.separator ''
 
           opts.on('-h', '--help', 'Show this message') do
-            puts opts
+            terminal.say opts.to_s
             exit
           end
 
@@ -101,14 +68,57 @@ BANNER
         end
       end
 
+      def update(game)
+        # get info
+        marble      = game.current_player.marble
+        x, y, s, d  = game.current_player.last_move
+
+        # output turn info & board
+        terminal.say "\nTurn #{game.turns_played}: Player #{marble} played: [#{x}, #{y}], rotated square: #{s} #{d}"
+        if game.game_over?
+          if game.tie_game?
+            terminal.say "Game ends w/state: --- TIE GAME ---"
+          else
+            terminal.say "Game ends w/state: --- PLAYER #{game.find_winner.marble} WINS! ---"
+          end
+        end
+
+        terminal.say "#{game.board}\n"
+      end
+
+      def ask_for_move(player, board)
+        terminal.say board if board.moves == 0
+        terminal.say "\nPlayer #{player.marble}. It's your Turn."
+
+        x, y = terminal.ask("Marble position? (x,y) ", lambda { |s|
+          s.split(',').map(&:to_i) }) do |q|
+          q.whitespace  = :remove
+          q.validate    = /[0-5],[0-5]/
+        end
+        
+        s = terminal.ask('Square to rotate? (0-3) ', String) do |q|
+          q.validate = /[0-3]/
+        end.to_i
+        
+        d = terminal.ask('Direction? (cw or ccw)', lambda { |s|
+          s == 'cw' ? :clockwise : :counter_clockwise }) do |q|
+          q.validate = /cw|ccw/
+          q.default  = 'cw'
+        end
+
+        [x, y, s, d]
+      end
+
       private
+      
+      attr_reader :terminal
 
       def options_valid?
         @options.keys.include?(:player1) && @options.keys.include?(:player2)
       end
 
       def output_usage
-        puts @options_parser
+        terminal.say @options_parser.to_s
       end
 
       def load_player(engine_name, player_marble)
@@ -121,35 +131,12 @@ BANNER
 
         player
       rescue Exception => e
-        raise TypeError, "#{e.message}\n#{e.backtrace}\n!! invalid player engine (#{engine_name})"
+        raise TypeError, "#{e.message}\n!! invalid player engine (#{engine_name})!"
       end
 
       def player_to_constant(name)
         camel = name.to_s.split('_').map { |s| s.capitalize }.join
         eval("Pentago::#{camel}Player")
-      end
-
-      def gets
-        @input ? @input.gets : ''
-      end
-
-      def say(message)
-        @output.puts(message) if @output
-      end
-
-      def ask(question)
-        say("#{question} [yn]: ")
-        gets.chomp.downcase == 'y'
-      end
-
-      def ask_coords(question)
-        say("#{question} [ex: 3,4]: ")
-        gets.chomp.split(',').map { |n| n.strip.to_i }
-      end
-
-      def ask_rotation(question)
-        say("#{question} [ex: 1,cw || 0,ccw]: ")
-        gets.chomp.split(',').map { |n| n.strip }.map{ |n| n.start_with?('c') ? n : n.to_i }
       end
     end
   end
